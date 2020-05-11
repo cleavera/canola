5275536463c131435111234147aa58b24c82b94c
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2020-05-11 13:39:53 +0100

    chore(age5): show gardeners required for seeds

--- packages/age5/src/overview/features/stock-value.feature.ts
+++ packages/age5/src/overview/features/stock-value.feature.ts
@@ -1,4 +1,4 @@
-import { Stocks, StocksRepository } from '@actoolkit/domain';
+import { Plants, Stocks, StocksRepository } from '@actoolkit/domain';
 
 import { OverlayComponentFactory } from '../../shared';
 import { LandSeedPlantsModel } from '../models/land-seed-plants.model';
@@ -7,10 +7,12 @@ export async function stockValueFeature(): Promise<void> {
     const lsp: LandSeedPlantsModel = LandSeedPlantsModel.ForCurrentPage();
 
     const stocks: Stocks = await new StocksRepository().get();
+    const plantedSeeds: Plants = stocks.seeds.plants();
 
     lsp.getSeeds().appendChild(OverlayComponentFactory('Value', `
         Sold: ${stocks.seeds.sold().toString()}</br>
-        Planted: ${stocks.seeds.plants().sold().toString()}
+        Planted: ${plantedSeeds.sold().toString()}</br>
+        Gardeners required: ${stocks.seeds.gardeners().toLocaleString('en')}
     `));
 
     lsp.getPlants().appendChild(OverlayComponentFactory('Value', `
