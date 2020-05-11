2624673b953ef997673e9b5a9bd5ae5619be3387
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2020-05-11 12:00:28 +0100

    chore(domain): expose plant value

--- packages/domain/src/classes/plants.ts
+++ packages/domain/src/classes/plants.ts
@@ -1,7 +1,15 @@
 import { CropType } from '../constants/crop-type.constant';
 import { ICropTypeMap } from '../interfaces/crop-type-map.interface';
+import { Funds } from './funds';
 
 export class Plants implements ICropTypeMap<number> {
+    private static readonly SOLD: ICropTypeMap<Funds> = {
+        [CropType.TREE]: new Funds(315),
+        [CropType.BUSH]: new Funds(175),
+        [CropType.FLOWER]: new Funds(42),
+        [CropType.GRASS]: new Funds(18)
+    };
+
     public [CropType.TREE]: number;
     public [CropType.BUSH]: number;
     public [CropType.FLOWER]: number;
@@ -12,5 +20,14 @@ export class Plants implements ICropTypeMap<number> {
         this[CropType.BUSH] = bush;
         this[CropType.FLOWER] = flower;
         this[CropType.GRASS] = grass;
+    }
+
+    public sold(): Funds {
+        return Funds.Sum(
+            Funds.Scale(Plants.SOLD[CropType.TREE], this[CropType.TREE]),
+            Funds.Scale(Plants.SOLD[CropType.BUSH], this[CropType.BUSH]),
+            Funds.Scale(Plants.SOLD[CropType.FLOWER], this[CropType.FLOWER]),
+            Funds.Scale(Plants.SOLD[CropType.GRASS], this[CropType.GRASS])
+        );
     }
 }
