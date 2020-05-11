6065b25266797a28363f1dc432e088d6c89d10f4
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2020-05-11 12:00:40 +0100

    chore(domain): allow converting seeds to plants

--- packages/domain/src/classes/seeds.ts
+++ packages/domain/src/classes/seeds.ts
@@ -1,6 +1,7 @@
 import { CropType } from '../constants/crop-type.constant';
 import { ICropTypeMap } from '../interfaces/crop-type-map.interface';
 import { Funds } from './funds';
+import { Plants } from './plants';
 
 export class Seeds implements ICropTypeMap<number> {
     private static readonly SOLD: ICropTypeMap<Funds> = {
@@ -8,6 +9,13 @@ export class Seeds implements ICropTypeMap<number> {
         [CropType.BUSH]: new Funds(75),
         [CropType.FLOWER]: new Funds(18),
         [CropType.GRASS]: new Funds(7)
+    };
+
+    private static readonly PLANTING_RATE: ICropTypeMap<number> = {
+        [CropType.TREE]: 0.97,
+        [CropType.BUSH]: 0.9,
+        [CropType.FLOWER]: 0.87,
+        [CropType.GRASS]: 0.85
     };
 
     public [CropType.TREE]: number;
@@ -28,6 +36,15 @@ export class Seeds implements ICropTypeMap<number> {
             Funds.Scale(Seeds.SOLD[CropType.BUSH], this[CropType.BUSH]),
             Funds.Scale(Seeds.SOLD[CropType.FLOWER], this[CropType.FLOWER]),
             Funds.Scale(Seeds.SOLD[CropType.GRASS], this[CropType.GRASS])
+        );
+    }
+
+    public plants(): Plants {
+        return new Plants(
+            Seeds.PLANTING_RATE[CropType.TREE] * this[CropType.TREE],
+            Seeds.PLANTING_RATE[CropType.BUSH] * this[CropType.BUSH],
+            Seeds.PLANTING_RATE[CropType.FLOWER] * this[CropType.FLOWER],
+            Seeds.PLANTING_RATE[CropType.GRASS] * this[CropType.GRASS]
         );
     }
 }
