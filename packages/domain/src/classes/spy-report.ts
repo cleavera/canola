3d2bf8bbefa99399ab25c568af81eb14c6c5efea
camface <seekoontz@gmail.com>
2022-05-14 22:14:16 -0400

    add information for who spy target has attacked

--- packages/domain/src/classes/spy-report.ts
+++ packages/domain/src/classes/spy-report.ts
@@ -16,8 +16,10 @@ export class SpyReport {
     public defenders: Array<CompanyName>;
     public activity: Activity;
     public arMod: ArMod;
+    public hasAttacked: Array<CompanyName>;
+    public hasAttackedInLastDay: Array<CompanyName>;
 
-    constructor(target: CompanyName, reports: Array<NewsReport>, defenders: Array<CompanyName>, activity: Activity, incoming: Array<MobNews>, outgoings: Array<MobNews>, arMod: ArMod) {
+    constructor(target: CompanyName, reports: Array<NewsReport>, defenders: Array<CompanyName>, activity: Activity, incoming: Array<MobNews>, outgoings: Array<MobNews>, arMod: ArMod, hasAttacked: Array<CompanyName>, hasAttackedInLastDay: Array<CompanyName>) {
         this.target = target;
         this.reports = reports;
         this.incoming = incoming;
@@ -25,6 +27,8 @@ export class SpyReport {
         this.defenders = defenders;
         this.activity = activity;
         this.arMod = arMod;
+        this.hasAttacked = hasAttacked
+        this.hasAttackedInLastDay = hasAttackedInLastDay
     }
 
     public static ForReports(reports: Array<NewsReport>, currentPointInTime: PointInTime, target: CompanyName): SpyReport {
@@ -32,6 +36,8 @@ export class SpyReport {
         const incoming: Array<MobNews> = [];
         const outgoings: Array<MobNews> = [];
         const defenders: Record<string, CompanyName> = {};
+        const hasAttacked: Record<string, CompanyName> = {};
+        const hasAttackedInLastDay: Record<string, CompanyName> = {};
         const activity: Array<PointInTime> = [];
 
         for (const report of reports) {
@@ -40,6 +46,19 @@ export class SpyReport {
             if (arMod === null && NewsReport.isBattle(report) && BattleReport.isDefendingSelf(report.content, target)) {
                 arMod = ArMod.AdjustForTime(ArMod.Max(), tickDifference);
             }
+            
+            if (NewsReport.isBattle(report) && BattleReport.isAttacking(report.content)) {
+                const mob: MobNews = report.content;
+                const idAttacked: CompanyName | null = this._getAttacking(mob);
+                
+                if (idAttacked !== null) {
+                    hasAttacked[idAttacked.id] = idAttacked;
+                }
+                
+                if (idAttacked !== null && tickDifference < 145) {
+                    hasAttackedInLastDay[idAttacked.id] = idAttacked;
+                }
+                
 
             if (NewsReport.isMob(report)) {
                 const mob: MobNews = report.content;
@@ -93,5 +112,19 @@ export class SpyReport {
         }
 
         return defender;
+    }
+        
+    private static _getAttacking(mob: MobNews): CompanyName | null {
+        if (mob.originalMob.type !== MobType.ATTACKING) {
+            return null;
+        }
+
+        let attacking: CompanyName = mob.originalMob.target;
+
+        if (mob.isOutgoing) {
+            attacking = mob.originalMob.target;
+        }
+
+        return attacking;
     }
 }
