553f5905f358a0b666102c477115ebf366e17bf1
camface <seekoontz@gmail.com>
2022-05-14 22:08:17 -0400

    add isAttacking

--- packages/domain/src/classes/battle-report.ts
+++ packages/domain/src/classes/battle-report.ts
@@ -13,4 +13,8 @@ export class BattleReport {
     public static isDefendingSelf(report: BattleReport, selfCompany: CompanyName): boolean {
         return report.type === MobType.DEFENDING && CompanyName.is(report.target, selfCompany);
     }
+    
+    public static isAttacking(report: BattleReport): boolean {
+        return report.type === MobType.ATTACKING;
+    }
 }
